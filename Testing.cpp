2806dd7dc378f47048bb35f8de6b8497de7f0621
Eleshz <edgarhammond@gmail.com>
2023-04-10 12:42:49 -0400

    TextTesting

--- Testing.cpp
+++ Testing.cpp
@@ -1,1 +1,17 @@
-﻿
+﻿#include <memory>  // for shared_ptr, __shared_ptr_access
+#include <string>  // for operator+, to_string
+
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Button, Horizontal, Renderer
+#include "ftxui/component/component_base.hpp"      // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for separator, gauge, text, Element, operator|, vbox, border
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+int main(void) {
+	using namespace ftxui;
+
+	text("I am a piece of text");
+
+}
