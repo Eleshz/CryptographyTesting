0843be7e29805e673efbf248aa04d9eff2129e82
Eleshz <edgarhammond@gmail.com>
2023-04-21 19:42:49 -0400

    WORKING ON IT -- SUPRISING AMOUNT OF PROGRESS

--- QuickSearch.cpp
+++ QuickSearch.cpp
@@ -15,72 +15,123 @@
 #include <utility>
 #include <functional>
 
-std::mutex NoYouDont;
+namespace fs = std::filesystem;
+
+int Num_ThreadsStarted = 0;
+
+bool AllThreadsStartedBool = false;
+
+std::string FinalFileLocation = "0";
+
+std::mutex iostream_pause;
+std::mutex intInput_pause;
+std::mutex FolderInput_pause;
+std::mutex FileInput_pause;
+std::mutex AllScannersHaveStarted;
+
+std::condition_variable WaitForThreads;
 
 std::deque<std::filesystem::path> FolderList;
-
-std::deque<std::filesystem::path> FileList;
-
-std::vector<int> ThreadSelection;
-
-std::deque<std::filesystem::path> FolderToPassToThread;
-
+std::deque<std::filesystem::path> FolderDistribution;
+std::vector<int> Thread_Available;
 std::vector<std::thread> ThisThread;
 
-std::string ScannerItem(std::string GivenFolder, int WhatThread);
+std::string ScannerItem(int WhatThread);
+int FileDistributor();
 
-int FileDistributor(std::deque<std::filesystem::path> &FolderListPass);
+std::string QuickSearch(std::string StartSearchLocation, std::string FileToBeFound, int NumberOfThreads){
 
-std::string QuickSearch(std::string StartSearchLocation, std::string FileToBeFound, int NumberOfThreads) {
-
-	namespace fs = std::filesystem;
-
-	ThreadSelection.resize(NumberOfThreads);
-
+	FolderList.resize(NumberOfThreads);
+	FolderDistribution.resize(NumberOfThreads);
+	Thread_Available.resize(NumberOfThreads);
 	ThisThread.resize(NumberOfThreads);
+	Num_ThreadsStarted = NumberOfThreads;
+
+	for (int i = 0; i < FolderDistribution.size(); i++) {
+		FolderDistribution[i] = "0";
+	}
 
 	for (int i = 0; i < ThisThread.size(); ++i) {
-		std::thread ScanThread(ScannerItem, "", i);
+		std::thread ScanThread(ScannerItem, i);
 		ThisThread[i] = std::move(ScanThread);
 	}
 
-
-	NoYouDont.lock();
+	iostream_pause.lock();
 	std::cout << "Made all threads!\n\n";
-	NoYouDont.unlock();
+	iostream_pause.unlock();
 
 	FolderList.push_front(StartSearchLocation);
 
-	std::thread Dis_Thread(FileDistributor, std::ref(FolderList));
+	std::unique_lock<std::mutex> ThreadLock(AllScannersHaveStarted);
+	while (!AllThreadsStartedBool) WaitForThreads.wait(ThreadLock);
+
+	std::thread Dis_Thread(FileDistributor);
 
 	std::cout << "Started FolderDistributor\n\n";
 
 	int ThisThreadSize = ThisThread.size();
-
-	for (int i = 0; i < ThisThread.size(); ++i) {
+	for (int i = 0; i < ThisThread.size(); ++i){
 		ThisThread[i].join();
-	}
-
-	Dis_Thread.join();
-
-	std::cout << "\n\nAll done! Found file at: ";
+	} Dis_Thread.join();
 
 	return "GOOD";
 }
 
-std::string ScannerItem(std::string GivenFolder, int WhatThread) {
+std::string ScannerItem(int WhatThread) {
+	//Waits for every thread to start before the file distributor starts
+	intInput_pause.lock(); --Num_ThreadsStarted; intInput_pause.unlock();
+
+	std::deque<std::filesystem::path> FileList;
+
+	if (Num_ThreadsStarted == 0) {
+		AllThreadsStartedBool = true;
+		WaitForThreads.notify_all();
+	} else;
+
+	while (FinalFileLocation == "0") {
+
+		if (FolderDistribution[WhatThread] == "0") {
+
+			for (const auto& file : fs::directory_iterator(fs::path(FolderDistribution[WhatThread]))) {
+
+				if (fs::is_directory(file)) {
+					std::cout << file << " <--- Folder \n";
+					FolderList.push_back(file);
+				}
+				else {
+					std::cout << file << " <--- File \n";
+					FileList.push_back(file.path());
+				}
+
+			}
 
 
 
-	return "blah";
+		} else
+		{
+			continue;
+		}
+
+	}
+
+
+
+	return "Ok";
 }
 
-int FileDistributor(std::deque<std::filesystem::path>& FolderListPass) {
-
-
-
-	std::cout << "File distributor woke up!\n";
-
+int FileDistributor() {
+	while (FinalFileLocation == "0") {
+		fs::path folder = FolderList.front();
+		if (folder.empty()) {
+			FolderList.pop_front();
+			continue;
+		}
+		for (auto i : Thread_Available) {
+			if (Thread_Available[i] == 1) {
+				FolderDistribution[i] = folder;
+			}
+		}
+	}
 	return 0;
 }
 
